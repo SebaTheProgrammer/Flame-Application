5f2afa9bbc9ce104165cf56ee6d7f4b273d6dc63
VryensSebastiaan <119673781+SebaTheProgrammer@users.noreply.github.com>
2024-09-10 11:07:30 +0300

    First base of animated Cuphead
    Just made a quick spritesheet cuphead in the main to test how everything works.
    Next on the agenda, make it in his own class and add gravity, input, collisions.
    
    (I'm on Erasmus so it's difficult to make time, but I hope to finish it at the end of this week!)

--- game_application/lib/main.dart
+++ game_application/lib/main.dart
@@ -1,7 +1,70 @@
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+
+class CupheadTheGame extends FlameGame {
+  late SpriteAnimationComponent cupHead;
+
+  int spriteSheetColumns = 4;
+  int spriteSheetRows = 4;
+  double spriteSheetWidth = 628;
+  double spriteSheetHeight = 756;
+
+  @override
+  Future<void> onLoad() async {
+    try {
+      final spriteSheetImage = await images.load('Cuphead.png');
+
+      final double frameWidth = spriteSheetWidth / spriteSheetColumns;
+      final double frameHeight = spriteSheetHeight / spriteSheetRows;
+
+      final spriteSheet = SpriteSheet(
+        image: spriteSheetImage,
+        srcSize: Vector2(frameWidth, frameHeight),
+      );
+
+      final List<Sprite> sprites = [];
+
+      for (int row = 0; row < spriteSheetRows; row++) {
+        for (int column = 0; column < spriteSheetColumns; column++) {
+          sprites.add(spriteSheet.getSprite(column, row));
+        }
+      }
+
+      final combinedAnimation = SpriteAnimation.spriteList(
+        sprites,
+        stepTime: 0.15,
+        loop: true,
+      );
+
+      cupHead = SpriteAnimationComponent()
+        ..animation = combinedAnimation
+        ..size = Vector2.all(100)
+        ..position = Vector2(100, 100);
+      add(cupHead);
+
+      print('Sprite animated with row transitions successfully.');
+    } catch (e) {
+      print('Error loading or error animating sprite: $e');
+    }
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    // Game logic update
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    // Game rendering
+  }
+}
 
 void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  var game = CupheadTheGame();
+  runApp(GameWidget(game: game)); // Use GameWidget to properly load the game
 }
